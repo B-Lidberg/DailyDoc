f75e3a9133800d4f9ac3d48e9f765f0fcc4e833c
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-04-15 14:18:47 -0400

    Clear Warning Dialog added

---
+++ app/src/main/java/com/lid/dailydoc/presentation/components/ClearButton.kt
@@ -0,0 +1,52 @@
+package com.lid.dailydoc.presentation.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.vector.ImageVector
+
+@Composable
+fun ClearButton(onAction: () -> Unit, icon: ImageVector) {
+    MaterialTheme {
+        Column {
+            val openDialog = remember { mutableStateOf(false) }
+
+            IconButton(onClick = { openDialog.value = true } ) {
+                Icon(icon, contentDescription = null)
+            }
+
+            if (openDialog.value) {
+                AlertDialog(
+                    onDismissRequest = {
+                        openDialog.value = false
+                    },
+                    title = {
+                        Text(text = "Are you sure?")
+                    },
+                    text = {
+                        Text("If Confirmed the note will be cleared, " +
+                            "however an overwrite will not occur until Saved. " +
+                            "To retain previous state, close and re-open the app")
+                    },
+                    confirmButton = {
+                        CustomButton(
+                            onClick = {
+                                onAction()
+                                openDialog.value = false
+                            },
+                            text = "Confirm"
+                        )
+                    },
+                    dismissButton = {
+                        CustomButton(
+                            onClick = { openDialog.value = false },
+                            text = "Dismiss"
+                        )
+                    }
+                )
+            }
+        }
+    }
+}
