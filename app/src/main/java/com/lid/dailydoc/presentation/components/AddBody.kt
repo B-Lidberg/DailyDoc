3b1f1c7a39df415b2cf49d1473c1f96a80cf8f4e
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-04-16 14:36:07 -0400

    animated Column for Symbols: Glitches as visible on navigation

--- app/src/main/java/com/lid/dailydoc/presentation/components/AddBody.kt
+++ app/src/main/java/com/lid/dailydoc/presentation/components/AddBody.kt
@@ -1,6 +1,9 @@
 package com.lid.dailydoc.presentation.components
 
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -11,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -20,11 +24,13 @@ import com.lid.dailydoc.data.model.bullet
 import com.lid.dailydoc.data.model.complete
 import com.lid.dailydoc.data.model.star
 
+@ExperimentalAnimationApi
 @Composable
 fun AddBody(
     bodyChange: (String) -> Unit,
     body: String,
 ) {
+    var isFocused by remember { mutableStateOf(true) }
     val focusRequester = remember { FocusRequester() }
     var textFieldValue by  remember { mutableStateOf(TextFieldValue(body)) }
     Row {
@@ -34,7 +40,10 @@ fun AddBody(
                 .padding(4.dp)
                 .background(colors.background)
                 .weight(1f)
-                .focusRequester(focusRequester),
+                .focusRequester(focusRequester)
+                .onFocusChanged {
+                    isFocused = !isFocused
+                },
             value = textFieldValue,
             onValueChange = {
                 textFieldValue = it
@@ -45,66 +54,74 @@ fun AddBody(
                 focusedBorderColor = colors.background,
                 unfocusedBorderColor = colors.background
             ),
-            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences)
+            keyboardOptions = KeyboardOptions(capitalization = KeyboardCapitalization.Sentences),
+
         )
-        Column(horizontalAlignment = Alignment.End) {
-            Button(
-                modifier = Modifier.size(45.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colors.background
-                ),
-                onClick = {
-                    textFieldValue = TextFieldValue("${textFieldValue.text}$star ")
-                    focusRequester.requestFocus()
-                    textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
-                },
+        AnimatedVisibility(
+                visible = isFocused,
+        ) {
+
+            Column(
+                horizontalAlignment = Alignment.End,
             ) {
-                Text(star)
-            }
-            Spacer(modifier = Modifier.padding(bottom = 10.dp))
-            Button(
-                modifier = Modifier.size(45.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colors.background
-                ),
-                onClick = {
-                    textFieldValue = TextFieldValue("${textFieldValue.text} $complete ")
-                    focusRequester.requestFocus()
-                    textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
-                },
-            ) {
-                Text(complete)
-            }
-            Spacer(modifier = Modifier.padding(bottom = 10.dp))
-            Button(
-                modifier = Modifier.size(45.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colors.background
-                ),
-                onClick = {
-                    textFieldValue = TextFieldValue("${textFieldValue.text}$arrow ")
-                    focusRequester.requestFocus()
-                    textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
-                },
-            ) {
-                Text(arrow)
-            }
-            Spacer(modifier = Modifier.padding(bottom = 10.dp))
-            Button(
-                modifier = Modifier.size(45.dp),
-                colors = ButtonDefaults.buttonColors(
-                    backgroundColor = colors.background
-                ),
-                onClick = {
-                    textFieldValue = TextFieldValue("${textFieldValue.text}  $bullet ")
-                    focusRequester.requestFocus()
-                    textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
-                },
-            ) {
-                Text(
-                    text = bullet,
-                    textAlign = TextAlign.Center
-                )
+                Button(
+                    modifier = Modifier.size(45.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colors.background
+                    ),
+                    onClick = {
+                        textFieldValue = TextFieldValue("${textFieldValue.text}$star ")
+                        focusRequester.requestFocus()
+                        textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
+                    },
+                ) {
+                    Text(star)
+                }
+                Spacer(modifier = Modifier.padding(bottom = 10.dp))
+                Button(
+                    modifier = Modifier.size(45.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colors.background
+                    ),
+                    onClick = {
+                        textFieldValue = TextFieldValue("${textFieldValue.text} $complete ")
+                        focusRequester.requestFocus()
+                        textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
+                    },
+                ) {
+                    Text(complete)
+                }
+                Spacer(modifier = Modifier.padding(bottom = 10.dp))
+                Button(
+                    modifier = Modifier.size(45.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colors.background
+                    ),
+                    onClick = {
+                        textFieldValue = TextFieldValue("${textFieldValue.text}$arrow ")
+                        focusRequester.requestFocus()
+                        textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
+                    },
+                ) {
+                    Text(arrow)
+                }
+                Spacer(modifier = Modifier.padding(bottom = 10.dp))
+                Button(
+                    modifier = Modifier.size(45.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = colors.background
+                    ),
+                    onClick = {
+                        textFieldValue = TextFieldValue("${textFieldValue.text}  $bullet ")
+                        focusRequester.requestFocus()
+                        textFieldValue = textFieldValue.copyWithCursorPosition(CursorPosition.End)
+                    },
+                ) {
+                    Text(
+                        text = bullet,
+                        textAlign = TextAlign.Center
+                    )
+                }
             }
         }
 
