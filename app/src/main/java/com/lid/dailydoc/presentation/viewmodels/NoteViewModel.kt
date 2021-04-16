f972be14db7da31dc3c241157949cc9ef9aa6220
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-04-16 09:14:47 -0400

    Cleaned up Code

--- app/src/main/java/com/lid/dailydoc/presentation/viewmodels/NoteViewModel.kt
+++ app/src/main/java/com/lid/dailydoc/presentation/viewmodels/NoteViewModel.kt
@@ -9,39 +9,15 @@ import com.lid.dailydoc.utils.getCurrentDateAsString
 import kotlinx.coroutines.*
 import java.lang.IllegalArgumentException
 
-class NoteViewModel(private val repository: NoteRepository) : ViewModel() {
+class NoteViewModel(repository: NoteRepository) : ViewModel() {
 
     val allNotes: LiveData<List<Note>> = repository.allNotes.asLiveData()
 
-    fun clearNotes() = viewModelScope.launch {
-        repository.clearNotes()
-    }
-
     @RequiresApi(Build.VERSION_CODES.O)
-    var date: String = getCurrentDateAsString(
+    var date: String = getCurrentDateAsString()
 
-    )
-    @RequiresApi(Build.VERSION_CODES.O)
-    fun setDate() {
-        date = getCurrentDateAsString()
-    }
+    val exists: LiveData<Boolean> = repository.exists(date).asLiveData()
 
-    var noteExists: Boolean = false
-    var exists: LiveData<Boolean> = repository.exists(date).asLiveData()
-
-    fun checkNoteExists() {
-        noteExists = repository.noteExists(date)
-    }
-    var cachedNote = Note(dateCreated = "Mon, Jan 15, 2001")
-
-    private fun getNote(): Note = repository.findNoteByDate(date)
-
-    fun cacheNote() {
-        viewModelScope.launch(Dispatchers.Default) {
-            checkNoteExists()
-            cachedNote = if (exists.value == true) getNote() else Note(date)
-        }
-    }
 }
     class NoteViewModelFactory(
         private val repository: NoteRepository,
