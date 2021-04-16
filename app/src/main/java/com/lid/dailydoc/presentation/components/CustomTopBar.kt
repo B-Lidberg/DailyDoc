19774cc8f0fcb128eabd641f47c41a5bd525420f
B-Lidberg <76635047+B-Lidberg@users.noreply.github.com>
2021-04-15 23:32:04 -0400

    Cleaned up Top Bar

--- app/src/main/java/com/lid/dailydoc/presentation/components/CustomTopBar.kt
+++ app/src/main/java/com/lid/dailydoc/presentation/components/CustomTopBar.kt
@@ -1,16 +1,12 @@
 package com.lid.dailydoc.presentation.components
 
-import android.widget.Button
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -19,13 +15,11 @@ fun CustomTopBar(headerText: String, button: @Composable () -> Unit) {
         modifier = Modifier
             .padding(8.dp)
             .fillMaxWidth(),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = Alignment.CenterVertically,
     ) {
         Text(
             modifier = Modifier
+                .fillMaxWidth()
                 .padding(bottom = 4.dp, top = 4.dp)
-                .align(Alignment.CenterVertically)
                 .weight(1f),
             text = headerText,
             style = MaterialTheme.typography.h5,
